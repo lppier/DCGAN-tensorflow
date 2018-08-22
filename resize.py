7d154cb594b3e5dbc6bf265717a9f89fc40ef453
Pier <madstrum@gmail.com>
2018-08-22 14:31:21 +0800

    128x128 dcgan

--- resize.py
+++ resize.py
@@ -1,12 +1,12 @@
 import os
 import cv2
 
-src = "./data/eosinophil"
-dst = "./data/eosinophil_resized"
+src = "./data/monocyte"
+dst = "./data/monocyte_resized"
 
-os.mkdir(dst)
+#os.mkdir(dst)
 
 for each in os.listdir(src):
     img = cv2.imread(os.path.join(src, each))
-    img = cv2.resize(img, (64, 64))
+    img = cv2.resize(img, (128, 128))
     cv2.imwrite(os.path.join(dst, each), img)
